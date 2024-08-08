c701771840c9c8fb82560ce6bd7398aa3751246e
Douglas Silva <paulodnunes2016@gmail.com>
2024-08-08 20:32:39 -0300

    Modificando o xadrez

--- Xadrez/Torre.cs
+++ Xadrez/Torre.cs
@@ -1,4 +1,5 @@
-﻿using tabuleiro;
+﻿using System.Runtime.InteropServices;
+using tabuleiro;
 namespace Xadrez
 {
     internal class Torre : Peca
@@ -23,65 +24,58 @@ namespace Xadrez
 
             //Acima
             pos.definirvalores(posicao.Linha - 1, posicao.Coluna);
+            while(Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha,pos.Coluna] = true;
+                if(Tab.peca(pos) != null && Tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.Linha = pos.Linha - 1;
+            }
+            //Abaixo
+            pos.definirvalores(posicao.Linha + 1, posicao.Coluna);
             while (Tab.PosicaoValida(pos) && PodeMover(pos))
             {
                 mat[pos.Linha, pos.Coluna] = true;
                 if (Tab.peca(pos) != null && Tab.peca(pos).cor != cor)
                 {
-                    //Faz a parada da torre 
                     break;
                 }
-                // Vai para próxima posição acima.
-                pos.Linha = pos.Linha - 1;
-
-                //Abaixo
-                pos.definirvalores(posicao.Linha + 1, posicao.Coluna);
-                while (Tab.PosicaoValida(pos) && PodeMover(pos))
-                {
-                    mat[pos.Linha, pos.Coluna] = true;
-                    if (Tab.peca(pos) != null && Tab.peca(pos).cor != cor)
-                    {
-                        //Força a parada em caso de uma peça adversária.
-                        break;
-                    }
-                    //Quando volta no while encrementa mais uma linha.
-                    pos.Linha = pos.Linha + 1;
-                }
-                //Direita
-                pos.definirvalores(posicao.Linha, posicao.Coluna + 1);
-                while (Tab.PosicaoValida(pos) && PodeMover(pos))
-                {
-                    mat[pos.Linha, pos.Coluna] = true;
-                    if (Tab.peca(pos) != null && Tab.peca(pos).cor != cor)
-                    {
-                        //Faz a parada da torre 
-                        break;
-                    }
-                    // Vai para próxima posição acima.
-                    pos.Coluna = pos.Coluna + 1;
-
-                }
-                //Esquerda
-                pos.definirvalores(posicao.Linha, posicao.Coluna - 1);
-                while (Tab.PosicaoValida(pos) && PodeMover(pos))
-                {
-                    mat[pos.Linha, pos.Coluna] = true;
-                    if (Tab.peca(pos) != null && Tab.peca(pos).cor != cor)
-                    {
-                        //Faz a parada da torre 
-                        break;
-                    }
-                    // Vai para próxima posição acima.
-                    pos.Coluna = pos.Coluna - 1;
-
-                }
-
-
-
-
-                return mat;
-
+                pos.Linha = pos.Linha + 1;
             }
+            //Direita
+            pos.definirvalores(posicao.Linha, posicao.Coluna + 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.peca(pos) != null && Tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.Linha = pos.Coluna + 1;
+            }
+            //Esquerda
+            pos.definirvalores(posicao.Linha, posicao.Coluna -1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.peca(pos) != null && Tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.Linha = pos.Coluna - 1;
+            }
+
+
+
         }
+
+
+          return mat;
+
+
+
     }
 }
+ 
